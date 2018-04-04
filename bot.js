7ec63d56fbe00b640bb35cd4dbc10451c0c76fd0
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-05 03:51:47 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,6 @@ client.on('message', message => {
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
     
-    let command = messsage.content.split(" ")[0];
-    command = command.slice(prefix.length);
-    
-    let args = message.content.split(" ").slice(1);
-    
     if (nessage.content.startsWith(prefix + "say")) {
         message.channel.sendMessage(args.join(" "));
     }
