cdc6b5ffa303a85a1a71d8de5b9df1234299ff49
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-05 23:15:18 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,10 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     
-    client.user.setGame('Gunakan Code | -');
+    client.user.setGame("Gunakan Code | -");
+    client.user.setStatus("Online");
 
+    
 });
 
 const prefix = "-";
