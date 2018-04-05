0d4379700ce1d8f9c4b9b5174c3551bed1be4338
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-05 13:11:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,9 @@ client.on('message', message => {
     let cont = message.content.slice(prefix.length).split(" ");
     let args = cont.slice(1);
     
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    
     let args = message.content.split(" ").slice(1);
 
 
