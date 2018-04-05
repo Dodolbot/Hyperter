8a052ebaebf999f86e20b786f2c84353c7616aea
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-05 11:28:22 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,19 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!args[0]) return message.channel.send("oof");
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
+}
+
+module.exports.help = {
+  name: "clear"
+}
+
 const prefix = "-";
 
 client.on('message', message => {
