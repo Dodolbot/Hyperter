bbc67c221d523d5efe0b16f3423be7b6cbd89ddc
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-05 13:12:37 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,11 +13,8 @@ client.on('message', message => {
     let sender = message.author;
     let cont = message.content.slice(prefix.length).split(" ");
     let args = cont.slice(1);
-    
     let command = message.content.split(" ")[0];
     command = command.slice(prefix.length);
-    
-    let args = message.content.split(" ").slice(1);
 
 
     if (msg === prefix + 'PING') { 
