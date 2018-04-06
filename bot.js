071e9b21d6b897b03b146c1289c785cfbf54e675
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-06 07:15:27 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     
-client.setGame("In 100 server");
-client.userStatus("Online");
+client.user.setGame("In 100 server");
+client.user.userStatus("Online");
     
 });
 
