3bc83d34c2980f99dce52a0d79333861070c2fbf
VerterID <38083452+VerterID@users.noreply.github.com>
2018-04-08 04:19:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,31 @@ client.on('message', message => {
 	   .addField('Latency',`${ping}ms`, true)
 	   .setColor('#33C1FF')
 	  message.channel.send({embed})
-	    
+	   
+    }
+	
+      if(msg === "help"){
+       message.reply("Usage: !report <user> <reason>");
+       return;
+      }
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!rUser) return errors.cantfindUser(message.channel);
+      let rreason = args.join(" ").slice(22);
+      if(!rreason) return errors.noReason(message.channel);
+
+      let reportEmbed = new Discord.RichEmbed()
+      .setDescription("Reports")
+      .setColor(orange)
+      .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+      .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+      .addField("Channel", message.channel)
+      .addField("Time", message.createdAt)
+      .addField("Reason", rreason);
+
+      let reportschannel = message.guild.channels.find(`name`, "reports");
+      if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+      reportschannel.send(reportEmbed);
+
     }
     
         if (msg ==='HALLO') { 
